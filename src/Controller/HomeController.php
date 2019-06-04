bf4cf662e86bff02164cc3360c82bcf2b83ad196
Viktor Huszar <viktor@eramba.org>
2019-06-04 15:25:39 +0200

    Added functionality for loading counters on home page from txt files

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -41,32 +41,49 @@ class HomeController extends AppController
         // - releases.txt
         //
         
+        $countersFiles = [
+            'downloads' => 'downloads.txt',
+            'community_users' => 'community_users.txt',
+            'enterprise_users' => 'enterprise_users.txt',
+            'releases' => 'releases.txt'
+        ];
+
         $counters = [
             'downloads' => [
                 'name' => __('Downloads in 2018'),
-                'value' => 5000,
+                'value' => 0,
                 'delay' => null,
                 'duration' => 2000
             ],
             'community_users' => [
                 'name' => __('Community Users'),
-                'value' => 6560,
+                'value' => 0,
                 'delay' => 100,
                 'duration' => 2000
             ],
             'enterprise_users' => [
                 'name' => __('Enterprise Users'),
-                'value' => 200,
+                'value' => 0,
                 'delay' => 200,
                 'duration' => 1500
             ],
             'releases' => [
                 'name' => __('Releases in 2018'),
-                'value' => 50,
+                'value' => 0,
                 'delay' => 300,
                 'duration' => 1500
             ]
         ];
+
+        foreach ($countersFiles as $key => $file) {
+            $filePath = WWW_ROOT . 'resources' . DS . 'home' . DS . 'counters' . DS . $file;
+
+            if (!file_exists($filePath)) {
+                continue;
+            }
+
+            $counters[$key]['value'] = file_get_contents($filePath);
+        }
 
         $this->set(compact('counters'));
     }
