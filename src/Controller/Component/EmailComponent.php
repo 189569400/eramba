32197d63c23d344f135e7a4797fdc0e295382f15
Viktor Huszar <viktor@eramba.org>
2019-06-11 15:27:59 +0200

    Added component for sending emails

---
+++ src/Controller/Component/EmailComponent.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Controller\Component;
+
+use Cake\Controller\Component;
+use Cake\Controller\ComponentRegistry;
+use Cake\Event\Event;
+use Cake\Mailer\Email;
+
+/**
+ * Email component
+ */
+class EmailComponent extends Component
+{
+    /**
+     * Default configuration.
+     *
+     * @var array
+     */
+    protected $_defaultConfig = [
+    	'sendTo' => 'support@eramba.org',
+    	'subject' => "E-mail from eramba website's contact form",
+    	'template' => ''
+    ];
+
+    public function sendEmail($name, $from, $vars = [])
+    {
+        $email = new Email('default');
+        $email->setEmailFormat('html')
+            ->setFrom([$from => $name])
+            ->setTo($this->getConfig('sendTo'))
+            ->setSubject($this->getConfig('subject'));
+
+        $email->viewBuilder()->setLayout('default');
+        $email->viewBuilder()->setTemplate($this->getConfig('template'));
+
+        $email->setViewVars($vars);
+
+        $email->send();
+    }
+}
