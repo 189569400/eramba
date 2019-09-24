a7e1b92a0a7c43f69e18e4a93196385a24b3054b
kisero <kisero@gmail.com>
2019-09-24 20:39:57 +0000

    updated price

--- src/Model/Table/ServicesTable.php
+++ src/Model/Table/ServicesTable.php
@@ -48,7 +48,7 @@ class ServicesTable extends Table
     public static function getVersions($version = null)
     {
         $versions = [
-            self::VERSION_PERM => __('On Premise'),
+            self::VERSION_PERM => __('On Premise - 2500 EUR'),
             // self::VERSION_SAAS => __('On SaaS')
         ];
 
